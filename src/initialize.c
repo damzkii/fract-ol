d0d618e41abf495370cb468728b3d1fa479d0a9c
Adam Hermawan <ahermawa@c2r6p11.hive.fi>
2022-10-03 18:02:24 +0300

    added error cases

--- src/initialize.c
+++ src/initialize.c
@@ -6,7 +6,7 @@
 /*   By: ahermawa <ahermawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 14:24:56 by ahermawa          #+#    #+#             */
-/*   Updated: 2022/10/03 17:10:41 by ahermawa         ###   ########.fr       */
+/*   Updated: 2022/10/03 18:01:36 by ahermawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,18 @@ void	error_msg(char *message, int error)
 void	init_mlx(t_mlx *mlx, t_img *img, t_zoom *zoom)
 {
 	mlx->mlx = mlx_init();
+	if (mlx->mlx == NULL)
+		error_msg("Error with mlx pointer", 1);
 	mlx->win = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "FRACTOL");
+	if (img->img == NULL)
+		error_msg("Window creation failure", 1);
 	img->img = mlx_new_image(mlx->mlx, WIDTH, HEIGHT);
+	if (!img->img)
+		error_msg("Creating new image failed!", 1);
 	img->data = mlx_get_data_addr(img->img, &img->bpp,
 			&img->stride, &img->endian);
+	if (!img->data)
+		error_msg("Getting the Image address failed!", 1);
 	screen_iteration(img, zoom);
 	mlx_put_image_to_window(mlx->mlx, mlx->win, img->img, 0, 0);
 	mlx_hook(mlx->win, 4, 0, mouse_hook, zoom);
